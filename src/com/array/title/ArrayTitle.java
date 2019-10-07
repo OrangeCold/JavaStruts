6c200e0cd8e5b8bff755e24f8d36dc4c0a8d7ebc
tom_glb <goodMorning_glb@atguigu.com>
2019-10-07 20:00:24 +0800

    将稀疏数组还原为普通数组

--- src/com/array/title/ArrayTitle.java
+++ src/com/array/title/ArrayTitle.java
@@ -7,6 +7,7 @@ public class ArrayTitle {
     /**
      * 稀疏数组:
      * 将普通数组转换为稀疏数组
+     * 再将稀疏数组还原
      */
     @Test
     public void commonTurnSparseArray(){
@@ -29,7 +30,6 @@ public class ArrayTitle {
             System.out.println(" ");
         }
 
-
         // 转化为稀疏数组
         int count = 0;
         for (int[] ints : com) {
@@ -48,15 +48,15 @@ public class ArrayTitle {
         sparse[0][1] = comCol;
         sparse[0][2] = count;
 
-        for (int k = 1; k < sparse.length; ) {
-            for (int i = 0; i < com.length; i++) {
-                for (int j = 0; j < com[i].length; j++) {
-                    if (com[i][j]!=0){
-                        sparse[k][0]=i;
-                        sparse[k][1]=j;
-                        sparse[k][2]=com[i][j];
-                        k++;
-                    }
+        // 记录是稀疏数组的第几行
+        int k = 0;
+        for (int i = 0; i < com.length; i++) {
+            for (int j = 0; j < com[i].length; j++) {
+                if (com[i][j]!=0){
+                    k++;
+                    sparse[k][0]=i;
+                    sparse[k][1]=j;
+                    sparse[k][2]=com[i][j];
                 }
             }
         }
@@ -68,6 +68,26 @@ public class ArrayTitle {
                 System.out.print(anInt + " ");
             }
 
+            System.out.println(" ");
+        }
+
+        // 还原稀疏数组
+        int[][] newCom = new int[sparse[0][0]][sparse[0][1]];
+        for (int[] ints : newCom) {
+            for (int anInt : ints) {
+                anInt = 0;
+            }
+        }
+
+        for (int i = 1; i < sparse.length; i++) {
+            newCom[sparse[i][0]][sparse[i][1]] = sparse[i][2];
+        }
+
+        System.out.println("还原后的数组：");
+        for (int[] ints : newCom) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
             System.out.println(" ");
         }
 
