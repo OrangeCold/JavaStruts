5fe8100d86830e36083224122073cf9dec618cb4
陈梓涵 <chenzihan.k@qq.com>
2019-12-25 11:13:06 +0800

    修改线程为兰姆达表达式

--- src/com/net/client/Client.java
+++ src/com/net/client/Client.java
@@ -8,21 +8,18 @@ import java.util.Scanner;
 public class Client {
 
     public static void main(String[] args) {
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    Socket socket = new Socket("127.0.0.1", 8888);
-                    OutputStream outputStream = socket.getOutputStream();
-                    while (true){
-                        Scanner scanner = new Scanner(System.in);
-                        String msg = scanner.nextLine();
-                        outputStream.write(msg.getBytes());
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8888);
+                OutputStream outputStream = socket.getOutputStream();
+                while (true){
+                    Scanner scanner = new Scanner(System.in);
+                    String msg = scanner.nextLine();
+                    outputStream.write(msg.getBytes());
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }.start();
+        }).start();
     }
 }
--- src/com/net/server/Server.java
+++ src/com/net/server/Server.java
@@ -2,47 +2,39 @@ package com.net.server;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8888);
-        new Thread(){
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Socket socket = serverSocket.accept();
-                        new Thread(){
-                            @Override
-                            public void run() {
-                                try {
-                                    InputStream inputStream = socket.getInputStream();
-                                    byte[] bytes = new byte[1024];
-                                    while (true) {
-                                        int len = 0;
-                                        while ((len=inputStream.read(bytes)) != -1){
-                                            String s = new String(bytes, 0, len);
-                                            System.out.println("s = " + s);
-                                        }
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    new Thread(() -> {
+                        try {
+                            InputStream inputStream = socket.getInputStream();
+                            byte[] bytes = new byte[1024];
+                            while (true) {
+                                int len = 0;
+                                while ((len=inputStream.read(bytes)) != -1){
+                                    String s = new String(bytes, 0, len);
+                                    System.out.println("s = " + s);
                                 }
                             }
-                        }.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
             }
-        }.start();
+
+        }).start();
     }
 
 }
