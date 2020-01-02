83173073ece243d77a6d12cb82dfc30f124bc8c5
陈梓涵 <chenzihan.k@qq.com>
2020-01-02 18:42:15 +0800

    力扣:三数之和

--- src/leetcode/primary/sort/Tittle.java
+++ src/leetcode/primary/sort/Tittle.java
@@ -1,8 +1,8 @@
 package leetcode.primary.sort;
 
-import org.junit.internal.runners.statements.FailOnTimeout;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tittle {
 
@@ -17,6 +17,45 @@ public class Tittle {
             j++;
         }
         Arrays.sort(nums1);
+    }
+
+    /**
+     * 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        int length = nums.length;
+
+        if (length < 3){
+            return null;
+        }
+
+        int l = 0;
+        int r = length-1;
+
+        boolean addL = true;
+
+        while ((r-l)>1){
+            for (int i = l+1; i < r; i++) {
+                if ((nums[l] + nums[i] + nums[r]) ==0){
+                    List<Integer> row = new ArrayList<>();
+                    row.add(nums[l]);
+                    row.add(nums[i]);
+                    row.add(nums[r]);
+                    result.add(row);
+                }
+            }
+            if (addL){
+                l++;
+                addL = false;
+            }else {
+                r--;
+                addL = true;
+            }
+        }
+
+        return result;
     }
 
 }
--- src/leetcode/thread/entity/FooBar.java
+++ src/leetcode/thread/entity/FooBar.java
@@ -1,53 +1,45 @@
 package leetcode.thread.entity;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * @author chenzihan
  * @date 2019-12-31 16:37
  */
 public class FooBar {
-    private volatile int n;
+    private int n;
 
-    private volatile boolean canFoo;
+    private Semaphore semaFoo = new Semaphore(1);
+    private Semaphore semaBar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
-        this.canFoo = true;
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
 
-        synchronized (this){
             for (int i = 0; i < n; i++) {
-                if (!canFoo) {
-                    this.wait();
-                }
+
                 // printFoo.run() outputs "foo". Do not change or remove this line.
+                semaFoo.acquire();
                 printFoo.run();
-                canFoo = false;
-                this.notifyAll();
+                semaBar.release();
+
             }
 
-        }
 
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
-        synchronized (this){
-
             for (int i = 0; i < n; i++) {
 
-                if (canFoo){
-                    this.wait();
-                }
-
                 // printBar.run() outputs "bar". Do not change or remove this line.
+                semaBar.acquire();
                 printBar.run();
-                canFoo = true;
-                this.notifyAll();
+                semaFoo.release();
 
             }
-        }
 
     }
 }
